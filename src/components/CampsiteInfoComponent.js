f451eb73529e3f8f1ca39e0d16d6f1045d0d1e80
Joshua Beadle <rebelmusicprods@gmail.com>
2021-07-30 13:38:31 -0600

    Header and Footer

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
 
-class CampsiteInfo extends Component {
-    constructor(props) {
-        super(props)
-    }
 
-    renderCampsite(campsite) {
-        return(
+function RenderCampsite({campsite}) {
+    return(
         <div className= "col-md-t m-1">
             <Card>
                 <CardImg top src={campsite.image} alt={campsite.name} />
@@ -20,7 +16,7 @@ class CampsiteInfo extends Component {
         );
     }
 
-    renderComments(comments){
+function RenderComments({comments}){
         if(comments) {
             return(<div className="col-md-5 m-1">
                 <h1>Comments</h1>
@@ -37,23 +33,20 @@ class CampsiteInfo extends Component {
         else <div></div>
     }
 
-    render() {
-        if (this.props.campsite) {
+  function CampsiteInfo(props) {
+        if (props.campsite) {
                 return (
                     <div className="container">
                         <div className= "row">
-                            {this.renderCampsite(this.props.campsite)}
-                            {this.renderComments(this.props.campsite.comments)}
+                            <RenderCampsite campsite={props.campsite} />
+                            <RenderComments comments={props.campsite.comments} />
                         </div>
                     </div>
                 )
             }
-            else {
-                return <div></div>
-            }
-    };
+                return <div></div>;
+    }
 
-}
 
 
 export default CampsiteInfo;
